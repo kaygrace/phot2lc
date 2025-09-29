5ab84e1dc19f63514f58393d8bed44e9bafdea9a
szsavery <143891445+szsavery@users.noreply.github.com>
2025-09-29 11:27:57 -0400

    Update photfunc.py

--- photfunc.py
+++ photfunc.py
@@ -255,7 +255,7 @@ def get_loc(hdr,telcode):
                 lat=47.9180556*u.deg,
                 height=944*u.m
             )
-    elif telcode == 'krak' or telescope == 'cdk500':
+    elif telcode == 'krak50' or telescope == 'cdk500':
         loc = EarthLocation.from_geodetic(
                 lon=20.0675000*u.deg,
                 lat=49.5691667*u.deg,
